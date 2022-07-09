d50a1d75edf3f8852ecf7a0d7d43b491518b67da
Aldair Zamora <aldair.zamora@outlook.com>
2022-07-08 21:11:44 -0600

    start with the html structure and firtst code in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const initialLoad = () => {
+  console.log("JS connected to HTML");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuseOptions = ["who", "action", "what", "when"];
+
+  const generatExcuse = () => {
+    let displayPhrase = ""; // variable to store the created excuse phrase
+    // The loop creates 5 different phrases
+    for (let i = 1; i < 6; i++) {
+      let randomArrayIndex = 0;
+      if (i < 5) {
+      }
+    }
+  };
 };
+
+window.addEventListener("DOMContentLoaded", initialLoad);
