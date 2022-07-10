185c2dc26f265cd6ac679a1cf796e341901c6f38
Aldair Zamora <aldair.zamora@outlook.com>
2022-07-09 22:20:48 -0600

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 const initialLoad = () => {
-  console.log("JS connected to HTML");
+	console.log("JS connected to HTML");
 
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+	let who = ["The dog", "My grandma", "His turtle", "My bird"];
+	let action = ["ate", "peed", "crushed", "broke"];
+	let what = ["my homework", "the keys", "the car"];
+	let when = [
+		"before the class",
+		"right on time",
+		"when I finished",
+		"during my lunch",
+		"while I was praying"
+	];
 
-  let excuseOptions = ["who", "action", "what", "when"];
+	let excuseOptions = ["who", "action", "what", "when"];
 
-  const generatExcuse = () => {
-    let displayPhrase = ""; // variable to store the created excuse phrase
-    // The loop creates 5 different phrases
-    for (let i = 1; i < 6; i++) {
-      let randomArrayIndex = 0;
-      if (i < 5) {
-      }
-    }
-  };
+	const generatExcuse = () => {
+		let displayPhrase = ""; // variable to store the created excuse phrase
+		// The loop creates 5 different phrases
+		for (let i = 1; i < 6; i++) {
+			let randomArrayIndex = 0;
+			if (i < 5) {
+			}
+		}
+	};
 };
 
 window.addEventListener("DOMContentLoaded", initialLoad);
